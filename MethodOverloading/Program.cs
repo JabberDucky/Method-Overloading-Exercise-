85613b806b732abe5e7fb68891fbbc9b6c68584a
Katherine Vail <jabberduckygame@gmail.com>
2023-05-18 11:43:33 -0500

    Completed exercise

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -4,7 +4,34 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+           /* var result = */ Add(1, 0, false);
+           // Console.WriteLine(result);
+        }
+        public static int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        public static double Add(double num1, double num2)
+        {
+            return num1 + num2;
+        }
+        public static void Add(int num1, int num2, bool ischecked)
+        {
+            var sum = num1 + num2;
+
+            if (ischecked == true && sum == 1)
+            {
+                Console.WriteLine($"{sum} dollar");
+            }
+            else if (ischecked == true && sum > 1)
+            {
+                Console.WriteLine($"{sum} dollars");
+            }
+            else
+            {
+                Console.WriteLine(sum);
+            }
         }
     }
 }
